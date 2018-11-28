9a48235373c7ce384098325eb73d6b0d3038f269
student <student@imac-15.local>
2018-11-28 09:41:55 +0800

    Initial Commit

---
+++ secondExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secondExam
+//
+//  Created by student on 2018/11/28.
+//  Copyright © 2018年 fanyunyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
